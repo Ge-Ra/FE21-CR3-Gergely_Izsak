d2d57b20713b71f7e9fbac75b5a074134a3ebd5e
Gergely Izsak <izsak.gera@gmail.com>
2021-06-26 09:22:48 +0200

    Finishing touches aaaaaand everything is ready now

--- JS/script.js
+++ JS/script.js
@@ -10,7 +10,7 @@ function calculateInvoice(
   return `You have to pay: ${sum} €`;
 }
 
-console.log(calculateInvoice(2, 10, 5, 3));
+console.log(calculateInvoice(2, 10, 5, 3)); /////// = 20 €
 
 // JS EXCERSISE-2
 
@@ -21,7 +21,7 @@ var darkishCoffey = 3;
 
 console.log(
   calculateInvoice(justNothing, normalBurger, trianglePancake, darkishCoffey)
-);
+); /////// = 20 €
 
 // JS EXCERSISE-3
 
@@ -32,7 +32,7 @@ var beveragePrice1 = 3;
 
 console.log(
   calculateInvoice(starterPrice1, maindishPrice1, dessertPrice1, beveragePrice1)
-);
+); /////// = 21 €
 
 let starterPrice2 = 4;
 let maindishPrice2 = 9;
@@ -41,7 +41,7 @@ let beveragePrice2 = 2;
 
 console.log(
   calculateInvoice(starterPrice2, maindishPrice2, dessertPrice2, beveragePrice2)
-);
+); /////// = 20 €
 
 const starterPrice3 = 2;
 const maindishPrice3 = 9;
@@ -50,7 +50,7 @@ const beveragePrice3 = 2;
 
 console.log(
   calculateInvoice(starterPrice3, maindishPrice3, dessertPrice3, beveragePrice3)
-);
+); /////// = 18 €
 
 // JS EXCERSISE-4
 
@@ -60,11 +60,11 @@ function studentInvoice(
   dessertPrice,
   beveragePrice
 ) {
-  let foodSum = starterPrice + maindishPrice + dessertPrice;
-  let discount10 = foodSum * 0.9 + beveragePrice;
-  return `10% discount applied. You have to pay: ${discount10} €`;
+  let foodSum = starterPrice + maindishPrice + dessertPrice;        //////I multiplied it with 0.9 because: I need 10% from 100%. 100% - 10% = 90%
+  let discount10 = (foodSum * 0.9) + beveragePrice;                 //////so I will just pay 90%, which is (x/100)*90 or (x*0.9)   
+  return `10% discount applied. You have to pay: ${discount10} €`;  //////e.g.: (1800/100)*90 = (18)*90 = 1620 or (18*0.9) = 16,2
 }
 
-console.log(studentInvoice(2, 10, 5, 2));
-console.log(studentInvoice(3, 10, 5, 3));
-console.log(studentInvoice(2, 9, 5, 3));
+console.log(studentInvoice(2, 10, 5, 2)); /////// = 17.3 € 
+console.log(studentInvoice(3, 10, 5, 3)); /////// = 19.2 €
+console.log(studentInvoice(2, 9, 5, 3)); /////// = 17.4 €
